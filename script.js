f288e07654b80eedbd89b31fe1ef01897dc78d82
xep097 <egilpett@me.com>
2025-03-04 02:30:03 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const updateCursor = ({ x, y }) => {
+  document.documentElement.style.setProperty('--x', x);
+  document.documentElement.style.setProperty('--y', y);
+}
+document.body.addEventListener('pointermove', updateCursor);
